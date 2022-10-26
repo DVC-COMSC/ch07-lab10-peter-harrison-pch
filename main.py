0cffa8977caf0c369028043a003d6f2726336b60
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 02:40:39 +0000

    test loop out

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # ******************************
 # Make your Code
 # ******************************
+# use nested for loops
+
+def switchem(lst1, index1, index2):
+    var1 = lst1.pop(index1)
+    var2 = lst1.pop(index2)
+    lst1.insert(index1, var2)
+    lst1.insert(index2, var1)
+
+lst = input().split()
+lst = list(map(int, lst))
+
+const = len(lst)
+for i in range(const):
+    mini = min(lst[i:])
+    minindex = lst.index(mini)
+    if minindex != i:
+            switchem(lst, i, minindex)
+    print(lst)
+        
